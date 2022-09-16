fdc229e3671828d60c3740a49fba2e79a643f244
Alysson Oliveira <3oalysson@gmail.com>
2022-09-16 14:55:19 -0300

    Update Transacoes.java

--- src/main/java/application/Transacoes.java
+++ src/main/java/application/Transacoes.java
@@ -1,6 +1,7 @@
 package application;
 
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,24 +16,33 @@ public class Transacoes {
         try{
             conn = DB.getConnection();
 
+            conn.setAutoCommit(false);
+
             st = conn.createStatement();
 
             int rows1 = st.executeUpdate("update seller" +
                     "set BaseSalary = 1090.00 where DepartmentId = 1");
-
-            int x = 1;
-            if (x < 2) {
-                throw new SQLException("Erro falso");
-            }
+            
+//            int x = 1;
+//            if (x < 2) {
+//                throw new SQLException("Erro falso");
+//            }
 
             int rows2 = st.executeUpdate("update seller" +
                     "set BaseSalary = 2090.00 where DepartmentId = 2");
+
+            conn.commit();
 
             System.out.println("rows1 " + rows1);
             System.out.println("rows2 " + rows2);
 
         } catch (SQLException e){
-            e.printStackTrace();
+            try {
+                conn.rollback();
+                throw new DbException("Transaction rolled back! Cause by: " + e.getMessage());
+            } catch (SQLException ex) {
+                throw new DbException("Error trying to rollback! Caused by: " + ex.getMessage());
+            }
         } finally {
             DB.closeStatement(st);
             DB.closeConnection();
