a7c7e95f70be7c66d5d93f3a7b709c04315946c0
Alysson Oliveira <3oalysson@gmail.com>
2022-09-16 19:03:59 -0300

    Classe Seller

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        System.out.println(seller);
     }
 }
