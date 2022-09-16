0e1b63ac3a867ee4bd3326dc669f9b74ebfe00f7
Alysson Oliveira <3oalysson@gmail.com>
2022-09-16 19:13:54 -0300

    Factory

---
+++ src/main/java/model/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
+
+}
