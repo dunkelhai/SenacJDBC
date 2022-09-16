abdc0eadba442f370ef4d59dae21d93b0094bf7c
Alysson Oliveira <3oalysson@gmail.com>
2022-09-16 14:35:03 -0300

    Create Atualizar.java

---
+++ src/main/java/application/Atualizar.java
@@ -0,0 +1,37 @@
+package application;
+
+import db.DB;
+
+import java.sql.*;
+
+public class Atualizar {
+    public static void main(String[] args) {
+
+        //ATUALIZAR DADOS
+
+        Connection conn = null;
+        PreparedStatement st = null;
+        try{
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "update seller " + 
+                            "set BaseSalary = BaseSalary + ? " +
+                            "where " +
+                            "(DepartmentId = ?)");
+            st.setDouble(1, 200.00);
+            st.setInt(2,2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+
+    }
+}
