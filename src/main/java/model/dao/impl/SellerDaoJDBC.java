08a3c62b09a411d1b7c1fc6fbe4fa7d32a944480
Alysson Oliveira <3oalysson@gmail.com>
2022-09-17 08:58:13 -0300

    Update SellerDaoJDBC.java

--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -3,9 +3,19 @@ package model.dao.impl;
 import model.dao.SellerDao;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void insert(Seller obj) {
 
@@ -23,7 +33,20 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement("" +
+                    "select seller.*, department.Name as DepName" +
+                    "from seller inner join department" +
+                    "on seller.DepartmentId = department.Id" +
+                    "where seller.Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+        } catch (SQLException e){
+            
+        }
+
     }
 
     @Override
