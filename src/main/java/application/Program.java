f7a6a5449312e602c93ca38de646f00d19c787a2
Alysson Oliveira <3oalysson@gmail.com>
2022-09-17 14:25:58 -0300

    feito update

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -13,27 +13,33 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("==== TESTE 1: seller findById ====");
-        Seller seller = sellerDao.findById(3);
-        System.out.println(seller);
+//        System.out.println("==== TESTE 1: seller findById ====");
+//        Seller seller = sellerDao.findById(3);
+//        System.out.println(seller);
 
-        System.out.println("\n==== TESTE 2: seller findByDepartment ====");
-        Department department = new Department(2, null);
-        List<Seller> list = sellerDao.findByDepartment(department);
-        for (Seller obj : list){
-            System.out.println(obj);
-        }
+//        System.out.println("\n==== TESTE 2: seller findByDepartment ====");
+//        Department department = new Department(2, null);
+//        List<Seller> list = sellerDao.findByDepartment(department);
+//        for (Seller obj : list){
+//            System.out.println(obj);
+//        }
+//
+//        System.out.println("\n==== TESTE 3: seller findAll ====");
+//        list = sellerDao.findAll();
+//        for (Seller obj : list){
+//            System.out.println(obj);
+//        }
+//
+//        System.out.println("\n==== TESTE 4: seller insert ====");
+//        Seller newSeller = new Seller(null, "Joana", "joana@gmail.com", new Date(), 4000.00, department);
+//        sellerDao.insert(newSeller);
+//        System.out.println("Inserted! New id = " + newSeller.getId());
 
-        System.out.println("\n==== TESTE 3: seller findAll ====");
-        list = sellerDao.findAll();
-        for (Seller obj : list){
-            System.out.println(obj);
-        }
-
-        System.out.println("\n==== TESTE 4: seller insert ====");
-        Seller newSeller = new Seller(null, "Joana", "joana@gmail.com", new Date(), 4000.00, department);
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! New id = " + newSeller.getId());
+//        System.out.println("\n==== TESTE 5: seller update ====");
+//        seller = sellerDao.findById(1);
+//        seller.setName("TEste Teste");
+//        sellerDao.update(seller);
+//        System.out.println("Updated! " + seller);
 
     }
 }
--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -57,7 +57,27 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller obj) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(
+                    "update seller " +
+                            "set Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " +
+                            "where id = ?");
 
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+
+            st.executeUpdate();
+
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
