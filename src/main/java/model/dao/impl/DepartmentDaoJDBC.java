c76d5b0465fd17022ff630dc7aaf026666a21b1d
Alysson Oliveira <3oalysson@gmail.com>
2022-09-17 15:52:28 -0300

    implementando o DepartmentDAO

---
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,102 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.sql.*;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("insert into department " +
+                    "(Name) " +
+                    "values (?) ",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, obj.getName());
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                if (rs.next()){
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            } else {
+                throw new DbException("Error! No rows affected!");
+            }
+
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
+    }
+
+    @Override
+    public void update(Department obj) {
+
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("update department " +
+                            "set Name = ?");
+
+            st.setString(1, obj.getName());
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected == 0){
+                throw new DbException("Error! No rows affected!");
+            }
+
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement("delete from department where Id = ?");
+
+            st.setInt(1, id);
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected == 0){
+                throw new DbException("Departamento inexistente!");
+            }
+
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
